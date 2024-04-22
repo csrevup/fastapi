91317e1e52c7c9ac61adea733ccfc4a0e6260ca1
csrevup <c@itsrevup.com>
2024-04-22 13:58:40 +0200

    Testing with DB consult

--- api_functions.py
+++ api_functions.py
@@ -3,13 +3,14 @@ import psycopg2
 ##Connection to DB
 connection_string = "postgresql://postgres:PzDnyALSXLNhGLazCwgxffGjIPNoxHAQ@monorail.proxy.rlwy.net:19847/railway"
 
-def get_query(name):
+def get_query():
     ##DB Connection
     try:
         conn = psycopg2.connect(connection_string)
         print("Connected to database!")
         cur = conn.cursor()
-        sql="select description from items2 where name = " + name
+        #sql="select description from items2 where name = " + name
+        sql="select description from items2 where name = 'Pau'"
         cur.execute(sql)
         item = cur.fetchone()
         print(item)
