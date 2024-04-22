acad429ca635b8c295c7ec4b9cee5231d2d036da
csrevup <c@itsrevup.com>
2024-04-22 12:01:38 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/first_test")
+@app.get("/first_test_api")
 async def root():
     return {"greeting": "Hello, World!", "message": "Welcome to FastAPI!"}
