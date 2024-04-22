98573a9e80ce64073338d4ff1838c9de4a23f9ae
csrevup <c@itsrevup.com>
2024-04-22 12:52:55 +0200

    Adding API file

--- api_functions.py
+++ api_functions.py
@@ -1,21 +1,20 @@
-from fastapi import FastAPI
 import psycopg2
 
 ##Connection to DB
 connection_string = "postgresql://postgres:PzDnyALSXLNhGLazCwgxffGjIPNoxHAQ@monorail.proxy.rlwy.net:19847/railway"
 
-def db_query (name):
+def get_query(name):
     ##DB Connection
     try:
-        conn = psycopg2.connect(dbname=dbname, user=dbuser, password=dbpassword, host=dbhost)
+        conn = psycopg2.connect(connection_string)
         print("Connected to database!")
+        cur = conn.cursor()
+        sql="select description from items2 where name = " + name
+        cur.execute(sql)
+        item = cur.fetchone()
+        print(item)
+    
     except psycopg2.Error as e:
         print("Error connecting to database:", e)
         exit()
 
-
-app = FastAPI()
-
-@app.get("/first_test_api")
-async def root():
-    return {"greeting": "Hello, World!", "message": "Welcome to FastAPI!"}
