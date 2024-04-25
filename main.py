687c5cc8cddf6e03d02647668434ab11df7412ca
csrevup <c@itsrevup.com>
2024-04-25 16:46:23 +0200

    Added Bearer Token Verification

--- main.py
+++ main.py
@@ -1,16 +1,35 @@
-from fastapi import FastAPI
-from api_functions import *
+from fastapi import FastAPI, Depends, HTTPException, status
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 from pydantic import BaseModel
+from api_functions import get_query
+import os
+
+app = FastAPI()
+token=os.environ.get('access_token')
+security = HTTPBearer()
 
 class Name(BaseModel):
     name: str
 
-app = FastAPI()
+def verify_token(credentials: HTTPAuthorizationCredentials):
+    if credentials:
+        token = credentials.credentials
+        if token == token:  # Replace with your method of validating the token
+            return True
+        else:
+            raise HTTPException(
+                status_code=status.HTTP_403_FORBIDDEN, detail="Invalid token"
+            )
+    raise HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED, detail="Authorization required"
+    )
 
 @app.get("/person_description")
-async def root(name: str):
+async def root(name: str, token: HTTPAuthorizationCredentials = Depends(security)):
+    verify_token(token)  # Verify token before processing
     return {"response": get_query(name)}
 
 @app.get("/testing")
-async def return_description(name: Name):
-    return {"description":get_query(name.name)}
+async def return_description(name: Name, token: HTTPAuthorizationCredentials = Depends(security)):
+    verify_token(token)  # Verify token before processing
+    return {"description": get_query(name.name)}
