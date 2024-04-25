549c024912603837a1d81231ca57cdcd7ede9e14
csrevup <c@itsrevup.com>
2024-04-25 23:04:44 +0200

    Fixed not referring to variable

--- main.py
+++ main.py
@@ -53,7 +53,7 @@ async def submit_car_part(piece_details: CarPartRequest, token: HTTPAuthorizatio
     ##return {piece_sku(piece_details.piece_name,piece_details.car_brand,piece_details.car_model,piece_details.car_year)}
 
 
-    test_var = "select dai FROM vehicle_parts WHERE brand_idf = "+car_brand
+    test_var = "select dai FROM vehicle_parts WHERE brand_idf = "+piece_details.car_brand+" and model_idf="+piece_details.car_model+" and year=2021 and line="+piece_details.piece_name
     return {"hola":test_var}
 
 
