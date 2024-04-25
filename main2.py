2459ace00a0a1aadba990fde1ef1e32bf32557b7
csrevup <c@itsrevup.com>
2024-04-25 16:31:37 +0200

    Added a backup Main File

---
+++ main2.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+from api_functions import *
+from pydantic import BaseModel
+
+class Name(BaseModel):
+    name: str
+
+app = FastAPI()
+
+@app.get("/person_description")
+async def root(name: str):
+    return {"response": get_query(name)}
+
+@app.get("/testing")
+async def return_description(name: Name):
+    return {"description":get_query(name.name)}
